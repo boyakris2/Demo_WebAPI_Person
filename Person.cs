c798604efd96ef2adf33f1d20fb61ef38e2d5543
boyakris2 <krishnaboya@gmail.com>
2025-03-10 23:49:44 -0400

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoWebAPI.Models
+{
+    public class Person
+    {
+        public int Id { get; set; } = 0;
+        public string FirstName { get; set; } = "";
+        public string MiddleName { get; set; } = "";
+        public string LastName { get; set; } = "";
+
+
+    }
+}
