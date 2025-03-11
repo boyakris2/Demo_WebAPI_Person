3008efc8d34083ea827b5596d0a52cfc6deb40a9
boyakris2 <krishnaboya@gmail.com>
2025-03-10 23:50:54 -0400

    Add files via upload

--- PeopleController.cs
+++ PeopleController.cs
@@ -1,69 +1,61 @@
-﻿using DemoWebAPI.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.VisualBasic;
-using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using DemoWebAPI.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace DemoWebAPI.Controllers
+namespace DemoWebAPI
 {
     [Route("api/[controller]")]
     [ApiController]
     public class PeopleController : ControllerBase
     {
-        List<Person> people = new List<Person>();
-
-        public PeopleController() {
-            people.Add(new Person
-            {
-                FirstName = "Krishna",
-                MiddleName = "FamilyMan",
-                LastName = "Punctual, Believe in Harwork, Broken heart",
-                Id = 1
-
-            });
-            people.Add(new Person
-            {
-                FirstName = "Manju",
-                MiddleName = "SweatHeart",
-                LastName = "Monster, Lier, Storm",
-                Id = 2
-            });
-            people.Add(new Person
-            {
-                FirstName = "Sunny",
-                MiddleName = "Smart",
-                LastName = "LazyButt, Could have done much better",
-                Id = 3
-            });
-            people.Add(new Person
-            {
-                FirstName = "Bunny",
-                MiddleName = "Bit confused",
-                LastName = "Idiot, No discipline, Improving",
-                Id = 4
-            });
+        List<person> people = new List<person>();
+        public PeopleController()
+        {
+         people.Add(new person { FirstName = "Krishna", MiddleName ="FamilyMan",
+         LastName="Punctual"})
+         people.Add(new person 
+                { FirstName = "Manju",
+                    MiddleName = "SweatHeart",
+                    LastName = "Monster"
+                })
+         people.Add(new person
+                { FirstName = "Sunny",
+            MiddleName = "Smart",
+            LastName = "LazyButt"
+         people.Add(new person
+         {
+             FirstName = "Bunny",
+             MiddleName = "Confused",
+             LastName = "Idiot"
+         })
         }
-
+        
         // GET: api/<PeopleController>
         [HttpGet]
-        public List<Person> Get()
+        //public IEnumerable<string> Get()
+        public List<person> Get()
         {
             return people;
         }
 
         // GET api/<PeopleController>/5
         [HttpGet("{id}")]
-        public Person Get(int id, [FromHeader] string firstName)
+        public string Get(int id)
         {
-            return people.Where(x => x.Id == id).FirstOrDefault();
+            return "value";
         }
 
         // POST api/<PeopleController>
         [HttpPost]
-        public void Post([FromBody]string value)
+        public void Post([FromBody] string value)
         {
-           //strings.Add(value);
+        }
+
+        // PUT api/<PeopleController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
         }
 
         // DELETE api/<PeopleController>/5
